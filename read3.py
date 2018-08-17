85db0876167a5fb65ff25586c06065fae998d70a
jianghao <2564811788@163.com>
2018-08-17 16:25:50 +0800

    add read3

---
+++ read3.py
@@ -0,0 +1,10 @@
+from readbaseInfo import *
+import pandas as pd
+import tushare as ts
+import os
+import matplotlib.pyplot as plt
+import datetime
+import math
+import tensorflow as tf
+import numpy as np
+import random
