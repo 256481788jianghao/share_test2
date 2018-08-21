73f44c9d2aede5739d092634f80f79c6b2fa32f9
jianghao <2564811788@163.com>
2018-08-21 16:09:03 +0800

    xjkdj

--- read4.py
+++ read4.py
@@ -1,4 +1,3 @@
-from readbaseInfo import *
 import pandas as pd
 import tushare as ts
 import os
@@ -9,13 +8,28 @@ import tensorflow as tf
 import numpy as np
 import random
 
+baseDir = './database2'
+todaytime = datetime.datetime.now()
+today_str = todaytime.strftime('%Y_%m_%d')
+stock_base_info_filename = baseDir+"/"+"baseInfo_"+today_str+".csv"
+stock_base_info = pd.read_csv(stock_base_info_filename,encoding='utf-8',index_col=0)
+
 
 class StockData:
     def __init__(self,codeStr):
         self.codeStr = codeStr
-        self.data = readData(codeStr)
-        print(self.data)
+        self.data = None
+        data_filename = baseDir+'/'+self._getCodeBySymbol(self.codeStr)+'.csv'
+        if os.path.exists(data_filename):
+            self.data = pd.read_csv(data_filename,encoding='utf-8',index_col=0)
+        else:
+            print(data_filename+' not exists')
+
+    def _getCodeBySymbol(self,codeStr):
+        code_int = int(codeStr)
+        codeR = stock_base_info[stock_base_info.symbol == code_int].ts_code.iloc[0]
+        return codeR
 
 
 
-StockData('300024')
+StockData('4')
