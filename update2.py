5e50f7ec9fa54a741ba20d189f7fb11ef1719ca3
jianghao <2564811788@163.com>
2018-08-28 15:02:58 +0800

    add date date

--- update2.py
+++ update2.py
@@ -13,10 +13,38 @@ if not os.path.exists(baseDir):
 ts.set_token('edd599506620c2fa4466f6ff765ff458d3dd894b136356c68b8baa32')
 ts_api = ts.pro_api()
 
+
+
 todaytime = datetime.datetime.now()
 today_str = todaytime.strftime('%Y_%m_%d')
+today_str2 = todaytime.strftime('%Y%m%d')
 
 
+#get trade cal
+stock_trade_cal_filename = baseDir+"/"+"trade_cal_"+today_str+".csv"
+stock_trade_cal_info = None
+if not os.path.exists(stock_trade_cal_filename):
+    stock_trade_cal_info_tmp = ts_api.trade_cal(start_date='20160101',end_date=today_str2)
+    stock_trade_cal_info_tmp.to_csv(stock_trade_cal_filename,encoding='utf-8')
+
+stock_trade_cal_info = pd.read_csv(stock_trade_cal_filename,encoding='utf-8',index_col=0)
+
+
+def get_stockdata_by_date(item):
+    if item.is_open == 1:
+        dateStr = str(item.cal_date)
+        stockdata_filename = baseDir+'/stock_'+dateStr+'.csv'
+        if not os.path.exists(stockdata_filename):
+            try:
+                data = ts_api.daily(trade_date=dateStr)
+                data.to_csv(stockdata_filename,encoding='utf-8')
+                print(stockdata_filename+' finish')
+            except Exception as e:
+                print(e)
+            
+stock_trade_cal_info.apply(get_stockdata_by_date,axis=1)
+
+'''
 
 #get all stock code list
 stock_base_info_filename = baseDir+"/"+"baseInfo_"+today_str+".csv"
@@ -81,7 +109,13 @@ for code in stock_base_info.ts_code:
         data.to_csv(data_filename,encoding='utf-8')
         
     print(code+" "+str(stock_update_index)+"/"+str(stock_len))
-
+'''
+'''
+exchange_id	str	交易所 SSE上交所 SZSE深交所
+cal_date	str	日历日期
+is_open	int	是否交易 0休市 1交易
+pretrade_date	str	上一个交易日
+'''
 
 '''
 ts_code	str	股票代码
